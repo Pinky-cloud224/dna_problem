07d97f3530b403a35ac2eabed6d2a36279a0d942
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-10-12 19:41:23 +0600

    Initial

---
+++ pythonic_way_array_number.py
@@ -0,0 +1,38 @@
+#PYTHONIC RULE FOR COUNTING
+###########################################
+#NUMBER1
+array=[2,2,4,56,9.1,2,0,1,2,98,5,43,1,7,6,4,3]
+
+two=2
+four=4
+
+if two and four in array:
+    print("Two and Four both exists")
+else:
+    print("Nothing")
+
+###############################################################################
+#NUMBER 2
+array=[2,2,4,56,9.1,2,0,1,2,98,5,43,1,7,6,4,3]
+
+two=2
+ten=10
+
+if two and ten in array:
+    print("Two and Ten both exists")
+else:
+    print("Nothing")
+#######################################################################################3
+
+#NUMBER 3
+
+array=[1.4,3.4,6.7,8.9,2.4,2.3]
+
+desimal1=1.4
+desimal2=8.9
+desimal3=2.4
+
+if desimal1 and desimal2 and desimal3 in array:
+    print("Desimals are Okay")
+else:
+    print("No Desimal")
